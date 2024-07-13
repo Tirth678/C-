01cec417bfe5929828e9fe49335c1f02474672a4
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-13 11:07:05 +0530

    commit

---
+++ CinOneShotCodeWithHarry/Chap-7/2.c
@@ -0,0 +1,5 @@
+// If S[3] is a 1-D array of integers then *(S+3) refers to the third element:
+// (i) True.
+// (ii) False.
+// (iii) Depends.
+// false it will point to
---
+++ CinOneShotCodeWithHarry/Chap-7/3.c
@@ -0,0 +1,2 @@
+// Write a program to create an array of 10 integers and store multiplication table of
+// 5 in it.
