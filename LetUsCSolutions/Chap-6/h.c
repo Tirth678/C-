a0f51091f1a6a32ed017b00f07733b666e4dae84
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-20 11:17:28 +0530

    commit

--- LetUsCSolutions/Chap-6/h.c
+++ LetUsCSolutions/Chap-6/h.c
@@ -1,13 +1,20 @@
-#include<stdio.h>
-int main(){
-    int am,pm,noon,midnight,timing;
-    printf("enter timings\n");
+#include <stdio.h>
+
+int main() {
+    int timing;
+    printf("Enter timings (0-23):\n");
     scanf("%d", &timing);
-    if(0>=timing || 12<=timing){
+
+    if (timing >= 0 && timing <= 12) {
         printf("morning\n");
-    }
-    else if(1>=timing || 4<=timing){
+    } else if (timing >= 13 && timing <= 16) {
         printf("afternoon\n");
+    } else if (timing >= 17 && timing <= 19) {
+        printf("evening\n");
+    } else if (timing >= 20 && timing <= 23) {
+        printf("night\n");
+    } else {
+        printf("Invalid input\n");
     }
 
     return 0;
