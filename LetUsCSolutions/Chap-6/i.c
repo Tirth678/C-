4b9a1eaff38f4133e019862f7b48cc2b7b81a093
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-20 11:59:31 +0530

    commit

---
+++ LetUsCSolutions/Chap-6/i.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n=1;
+    for(int i=0;i<10;i++){
+        for(int j=1;j<=n;j++){
+            printf("%d\n",i);
+            printf("\n");
+            printf("\n");
+        }
+        printf("\t");
+    }
+
+    return 0;
+}
+// solving
