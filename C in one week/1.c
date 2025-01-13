4ab6a00fb8ae07a79f098a3d5a1e9bcc67505d97
Tirth S Pandya <teamfznltd@gmail.com>
2025-01-13 15:06:03 +0530

    .

---
+++ C in one week/1.c
@@ -0,0 +1,13 @@
+// variables, constants and keywords
+// variable is a container which stores a value or a constant
+// its value can be changed
+a = 34;
+b = 34.4;
+c = "hi";
+// _ before the name
+// you cannot use any special
+// commans blank
+// you name them as logically as possible
+
+
+// constants are 
