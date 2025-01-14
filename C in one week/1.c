8ab0b4e09718abac473ab559825b4d8d5c9a4c09
Tirth S Pandya <teamfznltd@gmail.com>
2025-01-14 17:56:27 +0530

    .

--- C in one week/1.c
+++ C in one week/1.c
@@ -1,13 +1,6 @@
-// variables, constants and keywords
-// variable is a container which stores a value or a constant
-// its value can be changed
-a = 34;
-b = 34.4;
-c = "hi";
-// _ before the name
-// you cannot use any special
-// commans blank
-// you name them as logically as possible
-
-
-// constants are 
+#include<stdio.h>
+int main(){
+    int a = 45;
+    printf("%d", a);
+    return 0;
+}
